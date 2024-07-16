8e1ea306ea2727098d38ad77ffaf3957c8251873
Anna Terekhova <a.terekhova@dataduck.com>
2024-07-16 21:50:10 +0300

    anim

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
-import * as THREE from 'three';
-
+import * as THREE from "/node_modules/.vite/deps/three.js?v=b6edbe99";
+import { GLTFLoader } from "/node_modules/.vite/deps/three_addons_loaders_GLTFLoader__js.js?v=b6edbe99";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 camera.position.z = 5;
 
-function animate() {
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-
-}
+const loader = new GLTFLoader();
+loader.load('./sity.glb', function (gltf) {
+    const model = gltf.scene;
+    scene.add(model);
+}, undefined, function (error) {
+    console.error(error);
+});
